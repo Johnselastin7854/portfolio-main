3e0cbda7b00e6cf1c3a60d218dcb292d96a2aa0a
selastinmathew <selastinmathew@gmail.com>
2024-09-07 16:05:40 +0530

    hero section has been comepleted

--- assets/JS/animation.js
+++ assets/JS/animation.js
@@ -1,4 +1,4 @@
-/*<!--==================== Cursor Animation ====================--> */
+/**/
 // const cursor = document.getElementById("cursor");
 
 // document.addEventListener("mousemove", (e) => {
@@ -10,42 +10,89 @@
 //   });
 // });
 
-/*<!--==================== Navbar Animation ====================--> */
+/**/
 const open = document.querySelector(".nav__open");
 const close = document.querySelector(".nav__close");
+const navList = document.querySelector(".nav__list");
+const heroTitleTopDesktop = document.querySelector(".hero__title .desktop");
+const heroTitleBottomDesktop = document.querySelector(".bottom__left");
+console.log(heroTitleBottomDesktop);
+
+gsap.set(heroTitleBottomDesktop, {
+  opacity: 1,
+});
+gsap.set(heroTitleTopDesktop, {
+  opacity: 1,
+});
 
 let mm = gsap.matchMedia();
-const tl = gsap.timeline();
 
 mm.add("(max-width: 768px)", () => {
-  tl.to(".nav__list ", {
+  const mobileTl = gsap.timeline();
+
+  mobileTl.to(".nav__list", {
     right: 0,
     duration: 0.6,
   });
-  tl.from(".nav__list--item ", {
+  mobileTl.from(".nav__list--item", {
     x: 150,
     opacity: 0,
-    durarion: 0.7,
+    duration: 0.7,
     stagger: 0.25,
   });
-  tl.from(close, {
+  mobileTl.from(close, {
     y: -40,
     opacity: 0,
   });
 
-  tl.pause();
+  mobileTl.pause();
+
   open.addEventListener("click", () => {
-    tl.play();
-    close.style.display = "block";
+    mobileTl.play();
+    navList.style.display = "flex";
   });
   close.addEventListener("click", () => {
-    tl.reverse();
-    close.style.display = "none";
+    mobileTl.reverse();
   });
+
+  return () => {
+    mobileTl.kill();
+  };
 });
 
-mm.add("(min-width: 769px)", () => {});
-/*<!--==================== Hero Title Animation ====================--> */
+mm.add("(min-width: 769px)", () => {
+  const desktopTl = gsap.timeline();
+
+  desktopTl.from(".nav", {
+    y: -70,
+    opacity: 1,
+    durarion: 0.8,
+  });
+  desktopTl.from(".hero__title--top.desktop", {
+    y: 70,
+    opacity: 0,
+    duration: 0.6,
+  });
+
+  desktopTl.from(".bottom__left.desktop", {
+    y: 50,
+    opacity: 0,
+    duration: 0.6,
+  });
+
+  desktopTl.from(".bottom__right", {
+    y: 50,
+    rotate: 360,
+    opacity: 0,
+    duration: 0.5,
+  });
+
+  return () => {
+    desktopTl.kill();
+  };
+});
+
+/**/
 gsap.registerPlugin(ScrollTrigger);
 
 ScrollTrigger.create({
@@ -60,44 +107,17 @@ ScrollTrigger.create({
 
     gsap.to(".hero__title--left", {
       x: -progress * 200,
-      duration: 1,
+      duration: 0.8,
     });
 
     gsap.to("#line", {
       scaleX: progress * 2 + 1,
-      duration: 1,
+      duration: 0.8,
     });
 
     gsap.to(".hero__title--right", {
       x: progress * 200,
-      duration: 1,
+      duration: 0.8,
     });
   },
 });
-
-// document.addEventListener("DOMContentLoaded", () => {
-//   tl.from(".nav", {
-//     y: -70,
-//     opacity: 1,
-//     durarion: 0.8,
-//   });
-
-tl.from(".hero__title--top.desktop", {
-  y: 70,
-  opacity: 0,
-  duration: 0.6,
-});
-
-tl.from(".bottom__left.desktop ", {
-  y: 50,
-  opacity: 0,
-
-  duration: 0.6,
-});
-tl.from(".bottom__right ", {
-  y: 50,
-  rotate: 360,
-  opacity: 0,
-  duration: 0.5,
-});
-// });
